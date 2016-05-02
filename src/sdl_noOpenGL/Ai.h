18995c69956fc1d91777c404f5002d3b1b386b2f
Stefano Rinaldi <contact.stefano@gmail.com>
2016-05-02 14:32:23 +0100

    Added what AI class really needs to do.

--- src/sdl_noOpenGL/Ai.h
+++ src/sdl_noOpenGL/Ai.h
@@ -22,6 +22,20 @@ public:
 
 void Ai::aiUpdate(Player other)
 {
-	if (other.state._name == "stabMid")
-		stamina = 10;
+	// When player 1 is idle or defending, think about attacking
+
+		// Do a stamina check to see if we can attack
+			// if(stamina > 40)
+			// CREATE MOVE LOGGER FOR "OTHER" PLAYER
+		// Check which attack will be optimal based off of previous defense moves
+		// Make attack based off of these criteria
+
+	// When player 2 is attacking, think about the defending stance
+		
+		// Do a check against the blockChance (already kind of implemented)
+		// Randomise a possible response, based off of the check done above
+		// Delay the response until the attack in in motion (looks weird if you defend the instant they attack)
+		// Hold the block for a second and return to idle
+
+	// Oh shit, parry
 }
