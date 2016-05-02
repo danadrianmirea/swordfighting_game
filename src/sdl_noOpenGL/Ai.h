5c1c078ac87db5bfd60546b6cd14ea1108c88361
marlon <marlonkgilliam@gmail.com>
2016-05-02 17:13:08 +0100

    Improved Ai system.

--- src/sdl_noOpenGL/Ai.h
+++ src/sdl_noOpenGL/Ai.h
@@ -17,25 +17,57 @@ using namespace std;
 class Ai : public Player
 {
 public:
+	int delay = 20;
+	int dCount = 0;
+	int randomChance;
+
+	bool gaveitmybestshot = false;
+
+	int response = 5;
 	void aiUpdate(Player other);
+	void aiChoice(string state);
 };
 
-void Ai::aiUpdate(Player other)
+void Ai::aiUpdate(Player human)
 {
-	// When player 1 is idle or defending, think about attacking
+	if (human.state._name != "idle")
+		dCount++;
 
-		// Do a stamina check to see if we can attack
-			// if(stamina > 40)
-			// CREATE MOVE LOGGER FOR "OTHER" PLAYER
-		// Check which attack will be optimal based off of previous defense moves
-		// Make attack based off of these criteria
+	if (human.state._name == "idle" && (state._name == "blockMid" || state._name == "blockLow" || state._name == "blockHigh"))
+		state = idle;
 
-	// When player 2 is attacking, think about the defending stance
-		
-		// Do a check against the blockChance (already kind of implemented)
-		// Randomise a possible response, based off of the check done above
-		// Delay the response until the attack in in motion (looks weird if you defend the instant they attack)
-		// Hold the block for a second and return to idle
+	if (human.state._name != "idle"
+		&& state._name == "idle"
+		&& dCount >= delay
+		&& gaveitmybestshot == false) {
+		delay = rand() % response + 10;
+		dCount = 0;
+		aiChoice(human.state._name);
+	}
 
-	// Oh shit, parry
+	if (human.state._name == "idle") {
+		gaveitmybestshot = false;
+		dCount = 0;
+	}
+}
+
+void Ai::aiChoice(string playerState) {
+
+	gaveitmybestshot = true;
+	randomChance = rand() % 100 + 1;
+
+	if (playerState == "stabMid") {
+		if (randomChance <= blockMidChance)
+			state = blockMid;
+
+		successRateUpdate(blockMid);
+	}
+
+	if (playerState == "stabHigh") {
+		state = blockHigh;
+	}
+
+	if (playerState == "stabLow") {
+		state = blockLow;
+	}
 }
